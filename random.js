7b1f17cdfd6b92000a8191a96ce77a0c59349082
Haroenv <fingebimus@me.com>
2015-03-26 20:46:56 +0100

    shizzle

--- random.js
+++ random.js
@@ -1,14 +1,25 @@
-first = new Array(
+descriptions = new Array(
 	"student",
 	"git lover",
 	"open sourcer",
 	"programmer",
 	"nerd",
-	"space enthousiast"
+	"space enthousiast",
+	"poweruser"
 	);
 
+var oldIndex = 0;
+
 function generate() {
-	index1 = Math.floor(Math.random() * first.length);
-	var jobtitle = first[index1] ;
-	document.getElementById("generated").innerHTML = jobtitle;
+	index1 = Math.floor(Math.random() * descriptions.length);
+	if (index1 == oldIndex) {
+		if (index1 == 0) {
+			index1 = descriptions.length;
+		} else {
+			index1--;
+		};
+	};
+	var desc= descriptions[index1];
+	document.getElementById("generated").innerHTML = desc;
+	oldIndex = index1;
 }
